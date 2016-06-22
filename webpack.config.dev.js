1b1d91a24e16d179384c99474ead7b428c99354b
Keith Wade <keithwade@nanonation.net>
2016-06-22 13:19:29 -0500

    Webpack build things

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 module.exports = {
+  devtool: 'inline-source-map',
   entry: [
     './src/index.jsx'
   ],
@@ -24,9 +25,15 @@ module.exports = {
 			{
 				test: /\.(png|jpg)$/,
 				loader: 'url-loader?limit=8192'
-			}
+			},
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
 		]
   },
+  progress: true,
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
